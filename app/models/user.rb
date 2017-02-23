29b5ed0d87b811bfc5da58d7be5dc14ee610abee
sleeper <survivingknowledge@gmail.com>
2017-02-23 17:43:46 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ActiveRecord::Base
   # write associations here
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  # validates :name,
+  #           :password,
+  #           :presence => true
+
+
+  def mood
+    if self.happiness && self.nausea
+      self.happiness > self.nausea ? "happy" : "sad"
+    end
+  end
+
+  def num_of_rides
+    attractions.count
+  end
 end
