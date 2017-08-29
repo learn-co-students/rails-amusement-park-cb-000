9291e2a6672c4512b0d3381e2ec82970c3ec1770
tahbristol <tahbristol@gmail.com>
2017-08-28 23:00:16 -0400

    sign up as non-admin done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+
+  def current_user
+    session[:user_id] unless session[:user_id].nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+ resource :users, only: [:index, :new, :create, :edit, :update]
+ get '/users/:id', to: 'users#show'
+ resource :sessions, only: [:create]
 
-
+ get '/signin', to: 'sessions#new'
+ get '/welcome/home', to: 'welcome#home'
+ root 'welcome#home'
 end
