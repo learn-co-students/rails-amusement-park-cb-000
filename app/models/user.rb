e9688d52e6d3f0d84f0f2d2b64fff379cfa9abd1
Chris Franklin <cf73021@gmail.com>
2019-01-15 21:54:18 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,13 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides
   has_secure_password
-  enum :role => [:admin, :vip, :user]
 
+  #method to find whether the user is an admin or not.
+  def admin?
+    self.admin
+  end
+
+  #method to return the proper mood depending on whether the user has a higher nausea than happiness rating.
   def mood
     if self.nausea > self.happiness
       return 'sad'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-
-  devise_for :users
-  resources :users
+  resources :users, only: [:new, :create, :show]
+  get '/signin', to: 'sessions#new'
+  delete '/sessions', to: 'sessions#destroy'
+  resources :sessions, only: [:create]
+  resources :attractions
+  resources :rides, only: [:create]
   root 'welcome#home'
 end
