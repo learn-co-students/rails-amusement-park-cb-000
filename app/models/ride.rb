74d4c190a1406461adc3c69874f9bdcec0958aad
Aurangzaib <aurangzaib.danial@gmail.com>
2020-05-06 18:14:33 +0500

    passed model tests

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,40 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :attraction
+  belongs_to :user
+
+  def take_ride
+    if not_enough_tickets? && not_enough_height?
+      'Sorry. ' + tickets_message + ' ' + height_message
+    elsif not_enough_tickets?
+      'Sorry. ' + tickets_message
+    elsif not_enough_height?
+      'Sorry. ' + height_message
+    else
+      successful_ride
+    end
+  end
+
+  private
+  def tickets_message
+    "You do not have enough tickets to ride the #{attraction.name}."
+  end
+
+  def height_message
+    "You are not tall enough to ride the #{attraction.name}."
+  end
+
+  def not_enough_tickets?
+    user.tickets < attraction.tickets
+  end
+
+  def not_enough_height?
+    user.height < attraction.min_height
+  end
+
+  def successful_ride
+    user.tickets -= attraction.tickets
+    user.nausea += attraction.nausea_rating
+    user.happiness += attraction.happiness_rating
+    user.save
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140703140419) do
+ActiveRecord::Schema.define(version: 20200506120040) do
 
   create_table "attractions", force: :cascade do |t|
     t.string  "name"
@@ -28,11 +28,12 @@ ActiveRecord::Schema.define(version: 20140703140419) do
 
   create_table "users", force: :cascade do |t|
     t.string  "name"
-    t.string  "password"
+    t.string  "password_digest"
     t.integer "nausea"
     t.integer "happiness"
     t.integer "tickets"
     t.integer "height"
+    t.boolean "admin",           default: false
   end
 
 end
